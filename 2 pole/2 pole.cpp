65ae0d4cd3d424ba7b5481ecc0f668cfdc2cbf1e
Matouš Bien <matous.bien.020@pslib.cz>
2022-04-11 11:55:49 +0200

    Přidejte soubory projektu.

---
+++ 2 pole/2 pole.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+
+int main() {
+	int pole1[] = { 1, 2, 3, 4, 5 };
+	int pole2[] = { 2, 1, 8, 9, 9 };
+
+	for (int x = 0; x < 5; x++) {
+		printf("%d: %d\n", x, pole1[x]);
+
+		for (int x = 0; x < 5; x++) {
+			printf("%d: %d\n", x, pole2[x);
+
+
+			if (pole1 > pole2) {
+				printf("Pole 1 je vetsi nez pole 2.\n");
+			}
+
+			else printf("Pole 2 je vetsi nez pole 1.\n");
+		}
+	}
+}
